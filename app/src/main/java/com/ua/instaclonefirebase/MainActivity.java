58321cbec01e98b94369be0bd37bb69d5a182bec
Ugur Ayaz <ugurayaz23@gmail.com>
2017-11-13 23:17:28 +0300

    fix

---
+++ app/src/main/java/com/ua/instaclonefirebase/MainActivity.java
@@ -0,0 +1,24 @@
+package com.ua.instaclonefirebase;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
+}
